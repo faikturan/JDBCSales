814f0400cb4f7d89f6ff347fa70babe549ec750a
Faik TURAN <faikturan@users.noreply.github.com>
2017-01-12 18:32:16 +0300

    Add files via upload

--- ConnectionDB.java
+++ ConnectionDB.java
@@ -1,6 +1,8 @@
 package com.faikturan.common;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,7 +17,38 @@ public class ConnectionDB {
 		InputStream input = null;
 		try {
 			input = getClass().getResourceAsStream("/db.properties");
+			properties.load(input);
+			input.close();
+			if (properties.size()>0) {
+				log.info("Loaded connection parameters successfully");
+				return properties;
+			} else {
+				log.warn("Empty file");
+			}
 			
+		} catch (Exception ex) {
+			log.error("Some happened with the properties file", ex);
+		}
+		
+		return null;
+	}
+	
+	public Connection getConnection(){
+		Connection cn = null;
+		try {
+			Properties prop = loadProperties();
+			Class.forName(prop.getProperty("mysql.driver.ClassName"));
+			log.info("Driver registered successfully");
+			try {
+				cn = DriverManager.getConnection(
+						prop.getProperty("mysql.url")+
+						prop.getProperty("mysql.database"),
+						prop.getProperty("mysql.username"),
+						prop.getProperty("mysql.password"));
+				log.info("Connection established ");
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
